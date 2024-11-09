8a1b69c4f68c347248973b32e847b055c12fdc32
Denis Wanjohi <kazibin66@gmail.com>
2024-11-09 16:40:06 +0300

    update Mail service

--- Resources/user.py
+++ Resources/user.py
@@ -29,27 +29,27 @@ def generate_otp():
 
 def send_email(to_email, subject, content):
 
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login("kazibin66@gmail.com", "hougxifkgpbquqcy")
-        print("Connection successful!")
-        server.quit()
-    except Exception as e:
-        print("Error:", e)
-        
-    return
-    # Create the email message
-    # msg = MailMessage(subject=subject, recipients=[to_email], body=content)
     # try:
-    #     print("The /test route was accessed!")
-    #     print(f"2FA Email Body: {content}")
-    #     mail = current_app.extensions['mail']
-    #     mail.send(msg)
-    #     return True
+    #     server = smtplib.SMTP("smtp.gmail.com", 587)
+    #     server.starttls()
+    #     server.login("kazibin66@gmail.com", "hougxifkgpbquqcy")
+    #     print("Connection successful!")
+    #     server.quit()
     # except Exception as e:
-    #     print(f"Error sending email: {e}")
-    #     return False
+    #     print("Error:", e)
+        
+    # return
+    # Create the email message
+    msg = MailMessage(subject=subject, recipients=[to_email], body=content)
+    try:
+        print("The /test route was accessed!")
+        print(f"2FA Email Body: {content}")
+        mail = current_app.extensions['mail']
+        mail.send(msg)
+        return True
+    except Exception as e:
+        print(f"Error sending email: {e}")
+        return False
   
 class SignUpResource(Resource):
     # #    # Define the request payload schema for SignUp
